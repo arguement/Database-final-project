bc8c9cbe3db7981e55ebbd8330f4abe88f5ea97e
arguement <ultamatejordan@gmail.com>
2019-04-07 21:07:16 -0500

    sign up page added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,Email,EqualTo
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
+
+class SignUpForm(FlaskForm):
+    fname = StringField('first name', validators=[DataRequired()])
+    lname = StringField('last name', validators=[DataRequired()])
+    email = StringField('last name', validators=[DataRequired(),Email()])
+    password = PasswordField('New Password', [
+        DataRequired(),
+        EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
--- app/views.py
+++ app/views.py
@@ -3,12 +3,22 @@ from flask import render_template,request,redirect,url_for,flash
 from werkzeug.utils import secure_filename
 import os
 from app import mysql
-from app.forms import LoginForm
+from app.forms import LoginForm,SignUpForm
 
 @app.route("/")
 def home():
     form = LoginForm()
     return render_template("login.html",form= form)
+
+@app.route("/sign_up")
+def SignUp():
+    form = SignUpForm()
+    return render_template("signUp.html",form = form)
+
+@app.route("/home_page")
+def store():
+    return "home page"
+
 
 @app.after_request
 def add_header(response):
